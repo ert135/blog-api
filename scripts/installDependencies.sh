ac3e94727722f95312752ed835320e0c65c3734d
Robert Smith <smithr122@mc-s106531.local>
2017-11-21 19:20:20 +0000

    copied example appspec file

--- scripts/installDependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-sudo yum -y install nodejs
-sudo yum install 
--- scripts/setEnvVars.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export MONGODB_URL=mongodb://localhost/
-export JWT_SECRET=QRltddv345435H43HJ6
