d68fd93b0357adf68c661d0cba7bb91e4714ff44
Robert Smith <ert135@gmail.com>
2018-04-25 21:20:16 +0100

    Please enter the commit message for your changes. Lines starting

--- dist/utils/extractToken.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let extractToken = (req) => {
-    return req.body.token || req.query.token || req.headers['authorization'];
-};
-exports.extractToken = extractToken;
--- dist/utils/getSecret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getSecret = () => {
-    return process.env['JWT_SECRET'];
-};
-exports.getSecret = getSecret;
